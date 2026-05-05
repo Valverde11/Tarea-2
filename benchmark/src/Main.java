843dd7dd1a5a484cdffd49fdc90cd472164f93a1
Valverde11 <pablovalverdeduran@gmail.com>
2026-05-05 13:18:48 -0600

    chore:intengo arreglo

---
+++ benchmark/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
+        SwingUtilities.invokeLater(() -> {
+            BenchmarkFrame frame = new BenchmarkFrame();
+            frame.setVisible(true);
+        });
+    }
+}
--- benchmark/src/util/CSVExporter.java
+++ benchmark/src/util/CSVExporter.java
@@ -1,4 +1,7 @@
 
+
+
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
